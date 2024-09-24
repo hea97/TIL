ee08bc683f17a0976b9d7b726ff8f56a5e6f6f47
허태희 <s24054@gsm.hs.kr>
2024-09-24 20:25:30 +0900

    output code push

---
+++ Python/code/output.py
@@ -0,0 +1,17 @@
+# 하나만 출력
+print("# 하나만 출력")
+print("Hello World")
+print()
+
+# 여러개 출력
+print("# 여러개 출력")
+print("태희의", "Python", "TIL")
+print(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+print()
+
+# 줄바꿈 하기
+print("# 줄바꿈 하기")
+print("---확인 전용선---")
+print()
+print()
+print("---확인 전용선---")
