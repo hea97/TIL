5469590d6a908a6b750d83ad69ca5fac3b30c795
허태희 <s24054@gsm.hs.kr>
2024-09-24 20:53:02 +0900

    format push

---
+++ Python/code/format03.py
@@ -0,0 +1,14 @@
+# 양수
+output_f = "{:+d}".format(10)
+# 음수
+output_g = "{:+d}".format(-10)
+# 양수 : 기호 부분 공백
+output_h = "{: d}".format(10)
+# 음수 : 기호 부분 공백
+output_i = "{: d}".format(-10)
+
+print("# 기호와 함께 출력")
+print(output_f)
+print(output_g)
+print(output_h)
+print(output_i)
