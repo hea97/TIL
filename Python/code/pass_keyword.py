7a5b641c403be71a06eb83e01dc0d8d281b9a382
허태희 <s24054@gsm.hs.kr>
2024-09-24 21:16:47 +0900

    push

---
+++ Python/code/pass_keyword.py
@@ -0,0 +1,9 @@
+number = input("정수 입력> ")
+nubmer = int(number)
+
+if nubmer > 0:
+    
+    pass
+else
+
+		pass
---
+++ Python/code/pass_keywrod01.py
@@ -0,0 +1,7 @@
+number = input("정수 입력> ")
+nubmer = int(number)
+
+if nubmer > 0:
+    pass
+else:
+    pass
