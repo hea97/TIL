62f579ac1744c8e159e73b7ba7e5c4f2b84a12eb
허태희 <s24054@gsm.hs.kr>
2024-09-24 22:46:29 +0900

    iterator01 push

---
+++ Python/code/iterator01.py
@@ -0,0 +1,9 @@
+numbers = [1, 2, 3, 4, 5, 6]
+r_num = reversed(numbers)
+
+print("reversed_numbers : ", r_num)
+print(next(r_num))
+print(next(r_num))
+print(next(r_num))
+print(next(r_num))
+print(next(r_num))
