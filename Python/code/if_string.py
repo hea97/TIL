2d76f816861f04735de0d7ba65d9e6a28124d5b9
허태희 <s24054@gsm.hs.kr>
2024-09-24 22:40:54 +0900

    if string push

---
+++ Python/code/if_string.py
@@ -0,0 +1,11 @@
+number = int(input("정수 입력> "))
+
+if number % 2 == 0:
+	print("""\
+		입력한 문자열은 {}입니다.
+		{}는(은) 짝수입니다.""".format(number, number))
+
+else:
+	print("""\
+		입력한 문자열은 {}입니다.
+		{}는(은) 홀수입니다.""".format(number, number))
