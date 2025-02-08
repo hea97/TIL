6110161896bddd187a8f74d4c09cc7aa25d7759a
허태희 <s24054@gsm.hs.kr>
2025-02-08 18:53:08 +0900

    🚀 Upload Python code example

---
+++ Programming/Python/00_코드/output.py
@@ -0,0 +1,15 @@
+# 하나만 출력
+print("# 하나 출력")
+print("Hello")
+print()
+
+# 여러개 출력
+print("# 여러개 출력")
+print(10, 20, 30)
+print()
+
+# 줄 바꿈
+print("# 줄바꿈")
+print("---확인 전용선---")
+print()
+print("---확인 전용선---")
