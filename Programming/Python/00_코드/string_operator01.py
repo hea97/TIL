9b05b23adb59500a5e0c9b76a79af207122be0de
허태희 <s24054@gsm.hs.kr>
2025-06-09 21:10:14 +0900

    🔥 내용 전체 정리리

--- Programming/Python/00_코드/string_operator01.py
+++
@@ -1,6 +0,0 @@
-print("인덱싱에 대해")
-print("Hello"[0])
-print("Hello"[1])
-print("Hello"[2])
-print("Hello"[3])
-print("Hello"[4])
--- Programming/Python/00_코드/string_operator02.py
+++
@@ -1,6 +0,0 @@
-print("인덱싱에 대해2 (음수)")
-print("Hello"[-1])
-print("Hello"[-2])
-print("Hello"[-3])
-print("Hello"[-4])
-print("Hello"[-5])
